28e9b7b33ebe9c08ccec25ad84163f4341f4a443
Andrea Dottor <andrea@dottor.net>
2024-11-23 11:59:39 +0100

    Add blazor projects

---
+++ WPC.Dottor.BlazorNet9/WPC.Dottor.BlazorNet9.Client/Components/RedirectToLogin.cs
@@ -0,0 +1,11 @@
+﻿namespace WPC.Dottor.BlazorNet9.Client.Components;
+
+using Microsoft.AspNetCore.Components;
+
+public class RedirectToLogin (NavigationManager navigationManager) : ComponentBase
+{
+    protected override void OnInitialized()
+    {
+        navigationManager.NavigateTo($"Account/Login?returnUrl={Uri.EscapeDataString(navigationManager.Uri)}", forceLoad: true);
+    }
+}
---
+++ WPC.Dottor.BlazorNet9/WPC.Dottor.BlazorNet9/Components/Account/IdentityUserAccessor.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Identity;
+using WPC.Dottor.BlazorNet9.Data;
+
+namespace WPC.Dottor.BlazorNet9.Components.Account
+{
+    internal sealed class IdentityUserAccessor(UserManager<ApplicationUser> userManager, IdentityRedirectManager redirectManager)
+    {
+        public async Task<ApplicationUser> GetRequiredUserAsync(HttpContext context)
+        {
+            var user = await userManager.GetUserAsync(context.User);
+
+            if (user is null)
+            {
+                redirectManager.RedirectToWithStatus("Account/InvalidUser", $"Error: Unable to load user with ID '{userManager.GetUserId(context.User)}'.", context);
+            }
+
+            return user;
+        }
+    }
+}
